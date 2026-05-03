ea63ac3a40613f01f13f0e9db9ef86500687d556
es15326 <es15326@georgiasouthern.edu>
2026-05-03 14:44:38 -0500

    Add solution for iou-bounding-box

---
+++ iou-bounding-box/iou-bounding-box.py
@@ -0,0 +1,18 @@
+def iou(box_a, box_b):
+    """
+    Compute Intersection over Union of two bounding boxes.
+    """
+    # Write code here
+    x_left = max(box_a[0], box_b[0])
+    y_top = max(box_a[1], box_b[1])
+    x_right = min(box_a[2], box_b[2])
+    y_bottom = min(box_a[3], box_b[3])
+
+    intersection = max(0, x_right - x_left) * max(0, y_bottom - y_top)
+
+    area_a = (box_a[2] - box_a[0]) * (box_a[3] - box_a[1])
+    area_b = (box_b[2] - box_b[0]) * (box_b[3] - box_b[1])
+
+    union = area_a + area_b - intersection
+
+    return intersection / union
