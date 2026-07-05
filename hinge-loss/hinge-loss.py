2710b7f4a4cad533d3a5c499efe11489b8e08b61
es15326 <es15326@georgiasouthern.edu>
2026-07-05 17:48:05 -0500

    Solve hinge-loss

--- hinge-loss/hinge-loss.py
+++ hinge-loss/hinge-loss.py
@@ -9,10 +9,9 @@ def hinge_loss(y_true, y_score, margin=1.0, reduction="mean") -> float:
     """
     # Write code here
     y_true, y_score = np.array(y_true), np.array(y_score)
-
     loss = np.maximum(0, margin - (y_true * y_score))
 
     if reduction == "mean":
         return loss.mean()
-    else:
+    elif reduction == "sum":
         return loss.sum()
