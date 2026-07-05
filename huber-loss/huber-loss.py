67d18da04be011014f6a85e35cc3ff256fd2cd90
es15326 <es15326@georgiasouthern.edu>
2026-07-05 18:49:18 -0500

    Solve huber-loss

--- huber-loss/huber-loss.py
+++ huber-loss/huber-loss.py
@@ -6,9 +6,8 @@ def huber_loss(y_true, y_pred, delta=1.0):
     """
     # Write code here
     y_true, y_pred = np.array(y_true), np.array(y_pred)
+    e = np.abs(y_true - y_pred)
 
-    error = y_true - y_pred
-
-    loss = np.where(np.abs(error) <= delta, 0.5 * np.square(error), delta * (np.abs(error) - 0.5 * delta))
+    loss = np.where(e <= delta, 0.5 * e ** 2, delta * (e - 0.5 * delta))
 
     return loss.mean()
